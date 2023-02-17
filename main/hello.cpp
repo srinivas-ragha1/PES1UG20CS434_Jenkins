40db48c7d044b8918314eac553ff35579968e786
srinivas-ragha1 <79436730+srinivas-ragha1@users.noreply.github.com>
2023-02-18 00:21:23 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, This is Srinivas\n";
+  cout << "Hello, This is Srinivasa Raghavan\n";
   cout << "I have successfully built and run PES1UG20CS434\n";
   return 0;
 }
